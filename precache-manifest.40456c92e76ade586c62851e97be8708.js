a27040f52d71c4e37be3f53f91ee15bbb606d271
SimsonCult <63821433+SimsonCult@users.noreply.github.com>
2020-05-14 05:55:40 -0300

    Add files via upload

---
+++ precache-manifest.40456c92e76ade586c62851e97be8708.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bf8aaf30818b2386f40381c7c13ee71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0430fa8583b00bb91f8d",
+    "url": "/static/css/2.37865b2a.chunk.css"
+  },
+  {
+    "revision": "c27f7c8a4af26b286188",
+    "url": "/static/css/main.f47a0b4d.chunk.css"
+  },
+  {
+    "revision": "0430fa8583b00bb91f8d",
+    "url": "/static/js/2.652ba451.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.652ba451.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c27f7c8a4af26b286188",
+    "url": "/static/js/main.c194bd13.chunk.js"
+  },
+  {
+    "revision": "ecc823443bca06e9276a",
+    "url": "/static/js/runtime-main.bca38537.js"
+  },
+  {
+    "revision": "c66c92cea4decee3fbcf912cf71cbd10",
+    "url": "/static/media/Logo.c66c92ce.svg"
+  },
+  {
+    "revision": "44a18073ba6363180ea020efb99eee38",
+    "url": "/static/media/cart-empty.44a18073.svg"
+  },
+  {
+    "revision": "d7ac4f553f3f5bc842323e1ba8bf6d01",
+    "url": "/static/media/cart-full.d7ac4f55.svg"
+  },
+  {
+    "revision": "ebb5848c9a47fd1b781d8d04f94dfd7c",
+    "url": "/static/media/close.ebb5848c.svg"
+  },
+  {
+    "revision": "650cdc57dc0be3a71cc230a760215df5",
+    "url": "/static/media/garbage.650cdc57.svg"
+  }
+]);
